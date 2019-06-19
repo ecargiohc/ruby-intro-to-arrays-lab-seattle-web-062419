17e6afb06e21caf07b0de47001fd8052de08f92e
Grace Colette <ilovegrace09@gmail.com>
2019-06-19 07:08:54 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,15 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  array_with_two_elements = ["element1", "element2"]
+end
+
+def first_element(array)
+  array.first
+end
+
+def third_element(array)
+  array[3]
+end
